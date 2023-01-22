29f5b38cfd303692dab1fbdb453b43ac9d363649
Paul Schwarzenberger <paul@celidor.net>
2023-01-22 07:14:25 +0000

    allow outbound ICMP from EC2 instance

--- terraform-modules/vpc/main.tf
+++ terraform-modules/vpc/main.tf
@@ -129,6 +129,13 @@ resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.vpc.id
 
   egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
